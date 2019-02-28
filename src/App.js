6c7697fe1adbdf260a0661d1e25d6811a7544c85
svphan29 <36240410+svphan29@users.noreply.github.com>
2019-02-27 22:27:10 -0700

    set up basic css variables and fonts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Techbox
-      </div>
+      <React.Fragment>
+        TechBox
+      </React.Fragment>
     );
   }
 }
