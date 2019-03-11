0be6e042e7b0498cbdf644de520be048c148db57
svphan29 <36240410+svphan29@users.noreply.github.com>
2019-03-11 02:17:07 -0600

    add 404 handling

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import notFound from "../img/404.png";
 
 export default class Default extends Component {
   render() {
+    console.log(this.props);
     return (
-      <div>
-        <h3>page not found</h3>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-10 mx-auto text-center text-uppercase pt-5">
+            <h3>
+              the requested URL{" "}
+              <span className="text-danger">
+                {this.props.location.pathname}
+              </span>
+              {""} was not found
+            </h3>
+          </div>
+        </div>
+        <img style={{ width: "100%" }} src={notFound} alt="page not found" />
       </div>
-    )
+    );
   }
 }
