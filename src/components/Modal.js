dd3c0593ed91c684f43fb77a164fc344d5039f45
svphan29 <36240410+svphan29@users.noreply.github.com>
2019-03-10 19:30:43 -0600

    complete modal functionality

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import "./css/Product.css"
+import "./css/Product.css";
 import { ProductConsumer } from "../context";
 import { DetailsButton } from "./css/DetailsButton";
 import { Link } from "react-router-dom";
@@ -11,7 +11,7 @@ export default class Modal extends Component {
       <ProductConsumer>
         {value => {
           const { modalOpen, closeModal } = value;
-          const { img, title, price } = value.modalProduct;
+          const { img, title, price, company } = value.modalProduct;
 
           if (!modalOpen) {
             return null;
@@ -20,9 +20,39 @@ export default class Modal extends Component {
               <ModalContainer>
                 <div className="container">
                   <div className="row">
-                    <div id="modal"className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize p-5">
+                    <div
+                      id="modal"
+                      className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize p-5"
+                    >
                       <h5>Item added to cart!</h5>
-                      <img src={img} className="img-fluid modal-img" alt="product"/>
+                      <img
+                        src={img}
+                        className="img-fluid modal-img py-3"
+                        alt="product"
+                      />
+                      <h5>
+                        {company} {title}
+                      </h5>
+                      <h5 className="text-muted">Price: ${price}</h5>
+                      <div className="d-inline-block">
+                        <Link to="/productlist">
+                          <DetailsButton
+                            cart
+                            className="modal-btn"
+                            onClick={() => closeModal()}
+                          >
+                            Continue Shopping
+                          </DetailsButton>
+                        </Link>
+                        <Link to="/cart">
+                          <DetailsButton
+                            className="modal-btn"
+                            onClick={() => closeModal()}
+                          >
+                            Go To Cart
+                          </DetailsButton>
+                        </Link>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -42,7 +72,7 @@ const ModalContainer = styled.div`
   left: 0;
   right: 0;
   bottom: 0;
-  background: rgba(0, 0, 0, 0.3);
+  background: rgba(0, 0, 0, 0.2);
   display: flex;
   align-items: center;
   justify-content: center;
